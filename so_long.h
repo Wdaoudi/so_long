500126052e5e45f50657f2dff285756131b11121
Wassim Daoudi Idrissi <wdaoudi-@paul-f5Ar6s2.clusters.42paris.fr>
2024-09-22 14:54:11 +0200

    kk

--- so_long.h
+++ so_long.h
@@ -6,15 +6,15 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 12:46:11 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/09/20 11:18:27 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/09/22 13:23:22 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "./libft/libft.h"
-# include "./printf/ft_printf.h"
+// # include "./libft/libft.h"
+// # include "./printf/ft_printf.h"
 #include "./gnl/get_next_line.h"
 # include <fcntl.h>
 # include <limits.h>
